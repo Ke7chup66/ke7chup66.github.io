4d9519bd388138b26e81d9561f827ae67608b2aa
Ketchup <rookieketchup@gmail.com>
2025-10-29 20:33:51 +0800

    Update

--- JS/type.js
+++ JS/type.js
@@ -5,7 +5,8 @@ document.addEventListener('DOMContentLoaded',function(){
         "MAY THE PEACE BE WITH US",
         "任尔逆流万千苦,坚持无悔意无休",
         "专注自己,世界便鸦雀无声",
-        "一群打算上太空的疯子,我觉得还挺酷的"
+        "一群打算上太空的疯子,我觉得还挺酷的",
+        "你不能只在赢的时候，说爱你的国家"
     ];
 
     let currentTextIndex = 0;
